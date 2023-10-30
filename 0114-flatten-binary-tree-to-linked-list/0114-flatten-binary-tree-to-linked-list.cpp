2f0e0a316b78b77df93dcdf4e49e0b888121e3be
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2023-10-30 23:19:35 +0530

    Time: 3 ms (81.22%), Space: 13.4 MB (5.86%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -1,17 +1,25 @@
 
 class Solution {
 public:
+    vector<TreeNode*> preorder(TreeNode* root){
+        vector<TreeNode*> ans;
+        stack<TreeNode*> st;
+        if(root!=NULL) st.push(root);
+        while(st.size()>0){
+            TreeNode* temp=st.top();
+            st.pop();
+            ans.push_back(temp);
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left);
+        }
+        return ans;
+    }
     void flatten(TreeNode* root) {
-        if(root==NULL) return;
-        TreeNode* temp=root;
-        TreeNode* l = root->left;
-        TreeNode* r = root->right;
-        root->left = NULL;
-        root->right = NULL;
-        flatten(l);
-        flatten(r);
-        root->right=l;
-        while(temp->right!=NULL) temp=temp->right;
-        temp->right=r;
+        vector<TreeNode*> ans=preorder(root);
+        int n=ans.size();
+        for(int i=0;i<n-1;i++){
+           ans[i]->right=ans[i+1];
+           ans[i]->left=NULL;
+        }
     }
 };
