0ca2e1ee6a487d79cf0f2bf9321076f578b43309
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2023-11-19 01:06:36 +0530

    Time: 1037 ms (5.03%), Space: 177.2 MB (49.55%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[100005][105][2];
+    int ftb(vector<int> &v,int i,int k,bool on){
+    if(i==v.size()) return 0;
+    if(dp[i][k][on]!=-1) return dp[i][k][on];
+    int ans  = INT_MIN;
+    ans = ftb(v,i+1,k,on); // avoid wala case.
+    if(on){
+        // true means kharid liya ab bechna h no more.
+        ans = max(ans,v[i] + ftb(v,i+1,k-1,false)); // bechna
+    } else{
+        if(k>0) ans = max(ans,ftb(v,i+1,k,true)-v[i]);
+    }
+    dp[i][k][on]=ans;   
+    return ans;
+}
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof dp);
+        return ftb(prices,0,2,false);
+    }
+};
