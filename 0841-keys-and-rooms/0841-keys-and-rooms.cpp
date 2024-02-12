db5319c52f6e2597ef5242007b4a57e6a80fd648
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2024-02-12 13:51:34 +0530

    Time: 7 ms (75.17%), Space: 13.5 MB (32.05%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
+    vector<bool> vis;
+    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_set<int> visited;
-        queue<int> q;
-        q.push(0);
-        visited.insert(0);
-        while(q.size()!=0){
-            int temp=q.front();
-            q.pop();
-            for(auto it:rooms[temp]){
-                if(not visited.count(it)){
-                    q.push(it);
-                    visited.insert(it);
-                }
+        int n = rooms.size();
+        
+        vis.resize(n, false);
+        vis[0] = true; // Mark the first room as visited
+        
+        dfs(rooms, 0); // Start DFS from the first room
+        
+        // Check if all rooms are visited
+        for (bool visited : vis) {
+            if (!visited) {
+                return false;
             }
         }
-        return visited.size()==rooms.size();
+        
+        return true;
+    }
+    
+    void dfs(vector<vector<int>>& rooms, int currentRoom) {
+        for (int nextRoom : rooms[currentRoom]) {
+            if (!vis[nextRoom]) {
+                vis[nextRoom] = true;
+                dfs(rooms, nextRoom);
+            }
+        }
     }
 };
