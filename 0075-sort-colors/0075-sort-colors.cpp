bd0ba961a747fc549a9c4dfbd457765e1313d9ce
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2024-05-04 13:17:03 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (71.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& array) {
-        int n=array.size();
-      int zero_count = 0, one_count = 0,two_count=0;
-      for (int i = 0; i < n; i++) {
-        if (array[i] == 0) zero_count++;
-        else if(array[i]==1) one_count++;
-        else two_count++;
-      }
-      for (int i = 0; i < n; i++) {
-            if (zero_count) {
-              array[i] = 0;
-              zero_count--;
+       int l = 0, m = 0, h = array.size() - 1;
+       while (l <= h) {
+            if (array[l] == 2) {
+            swap(array[l], array[h]);
+            h--;
             }
-            else if(one_count) {
-              array[i] = 1;
-              one_count--;
+            else if (array[l] == 0) {
+              swap(array[m], array[l]);
+              m++;
+              l++;
             }
-            else{
-              array[i]=2;
-              two_count--;
+            else {
+              l++;
             }
         }
     }
